38be506d2d936634c12f4e4c0de2bb396fceeece
burbano444 <burbo444@hotmail.com>
2023-05-30 20:03:58 -0500

    Automation HelloWorld

---
+++ cypress/e2e/init.cy.js
@@ -0,0 +1,10 @@
+describe('holaMundoAutomationTest', () => {
+  it('landingPage demoblaze', () => {
+    cy.visit('')
+  })
+  it('login into demoblaze', () => {
+    cy.visit('/prod.html?idp_=1')
+
+  })
+  
+})
